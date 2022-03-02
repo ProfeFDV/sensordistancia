01368e65c6ae642f143914ccdd7e596f057601a0
Fran Diaz <100407685+ProfeFDV@users.noreply.github.com>
2022-03-02 09:59:51 +0100

    Muestra la distancia medida por el sensor, en la pantalla

--- main.ts
+++ main.ts
@@ -1,3 +1,11 @@
+let distancia = 0
 basic.forever(function () {
-	
+    distancia = sonar.ping(
+    DigitalPin.P2,
+    DigitalPin.P1,
+    PingUnit.Centimeters
+    )
+    basic.showNumber(distancia)
+    basic.pause(3000)
+    basic.clearScreen()
 })
